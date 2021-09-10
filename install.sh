27910a5c6028a4c708dbb89fe9ea2eb92f54e012
Mark Huang <mark.huang1212@gmail.com>
2021-09-10 10:52:14 +0800

    fix bugs in install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 # Run this script to deploy code-grader on a Ubuntu machine
 
-USER=$(id -un)
-
 sudo apt-get update
 sudo apt-get -y upgrade
 
 sudo apt-get install docker.io
-sudo usermod -aG docker 
+sudo usermod -aG docker $USER
 
 docker-compose build 
 docker-compose up
